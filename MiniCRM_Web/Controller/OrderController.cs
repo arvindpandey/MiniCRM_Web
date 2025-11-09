538118c330b15edc0b7e3bc7fe38bfd66985a992
bhagy <bhagy@HP>
2025-11-09 21:51:41 +0530

    Oreder Controller Created Sucessfully

---
+++ MiniCRM_Web/Controller/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniCRM_Web.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+    }
+}
